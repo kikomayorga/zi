d837fbd480f38805faabf0b0eb2f38b2d06b47e5
kikomayorga <kikomayorga@gmail.com>
2023-03-06 11:38:56 -0500

    learning lua+

--- learning/lua/tableappend.lua
+++
@@ -1,9 +0,0 @@
-require "tablesaveload"
-
-readtable = {}
-
-readtable = table.load("usuarios.db")
-
-readtable[#readtable +1] = {"usuario_N", "1234", "user"}
-
-table.save(readtable, "usuarios.db")
--- learning/lua/zi_functions.lua
+++ learning/lua/zi_functions.lua
@@ -1,36 +1,28 @@
 function users_db_reset(db_file)
-  users_table = {}
+  t = {}
   for i = 1, 6 do
-    users_table[i] = {
-      -- ["user_ID"]="u0x",
-      -- ["user_name"]="user_x",
+    t[i] = {
       ["password"]="1234", 
       ["running"]=0, 
       ["daily_quota"]=180,
       ["time_left_today"]=180,
-      ["assigned_device"]=nil,
+      ["assigned_device"]=0,
       ["own_devices_nr"]=0,
-      ["own_dev_1"]=nil,
-      ["own_dev_2"]=nil, 
-      ["own_dev_3"]=nil, 
+      ["own_dev_1"]=0,
+      ["own_dev_2"]=0, 
+      ["own_dev_3"]=0, 
       ["morning_start"]=360, -- 6hrs
       ["day_end"]=1260, --21 hrs
      }
   end
 
-  users_table[1].user_name ="usuario 1"
-  users_table[2].user_name ="usuario 2"
-  users_table[3].user_name ="usuario 3"
-  users_table[4].user_name ="usuario 4"
-  users_table[5].user_name ="usuario 5"
-  users_table[6].user_name ="usuario 6"
+  for i=1, 6 do t[i].user_name ="usuario "..i end
+  table.save(t, db_file)
 
-  -- table.save(users_table, "users_table.db")
-  table.save(users_table, db_file)
 return 1
 end
 
-function devices_db_reset(o)
+function devices_db_reset(db_file)
   devices_table = {}
   for i = 1, 32 do
     devices_table[i] = {
@@ -38,43 +30,78 @@ function devices_db_reset(o)
       ["id"]="d"..string.format("%02d",i)
      }
   end
-  table.save(devices_table, o)
+  table.save(devices_table, db_file)
 return 1
 end
 
 function rename_user(db_file, userID, new_name)
-t = table.load("users_table.db")
-t[userID]["user_name"]=new_name
-table.save(t, db_file)
+  t = table.load(db_file)
+  t[userID]["user_name"]=new_name
+  table.save(t, db_file)
 return 1
 end 
 
-function say_users(db_file)
-return 1
-end 
-
-function append_user(db_file)
-return 1
+function change_password(db_file, userID, new_password)
+  t = table.load(db_file)
+  t[userID]["password"]=new_password
+  table.save(t, db_file)
 end
 
-function change_password(db_file, userID)
--- validar password 
-end
+--[[
+function assign_device_permanently(db_file, deviceNR, userID)
+  t = table.load(db_file)
+  t[userID]["own_devices_nr"]=t[userID]["own_devices_nr"] + 1
+  if t[userID]["own_devices_nr"] = 1 then
+    t[userID]["own_device_1"]=deviceNR
+  end
 
-function assign_device_permanently(deviceNR, userID)
+  if t[userID]["own_devices_nr"] = 2 then
+    t[userID]["own_device_2"]=deviceNR
+  end
+
+  if t[userID]["own_devices_nr"] = 3 then
+    t[userID]["own_device_3"]=deviceNR
+  end
+
+  if t[userID]["own_devices_nr"] = 4 then
+    return 0
+  end
+  
+  table.save(t, db_file)
+end
+]]
+
+--[[
+function disassign_device_permanently(db_file, deviceNR, userID)
+  t = table.load(db_file)
+  if t[userID]["own_devices_nr"] = 0 then
+    return 0
+  end
+  if t[userID]["own_devices_nr"] = 3 then
+    t[userID]["own_device_3"]=0
+    t[userID]["own_devices_nr"]=2
+  end 
+
+  if t[userID]["own_devices_nr"] = 2 then
+    t[userID]["own_device_2"]=0
+    t[userID]["own_devices_nr"]=1
+  end
+
+  if t[userID]["own_devices_nr"] = 1 then
+    t[userID]["own_device_1"]=0
+    t[userID]["own_devices_nr"]=0
+  end
+
+  table.save(t, db_file)
+end
+]]
+
+function free_device_permanently(deviceNR)
   -- db_file, 
 end
 
 function mark_device_shared(deviceNR)
   -- 
-end
-
-function free_device_permanently(deviceNR)
-  -- db_file, 
-end
-
-function free_device_permanently(deviceNR)
-  -- db_file?
 end
 
 function name_device(deviceNR)
@@ -84,6 +111,16 @@ end
 function name_vehicle(db_file, insects_db_file)
 end
 
+-- just and example, unused!
 function createDir (dirname)
   os.execute("mkdir " .. dirname)
+end
+
+-- unused???
+function append_user(db_file)
+  t = {}
+  t = table.load(db_file)
+  t[#t +1] = {"usuario_N", "1234", "user"}
+  table.save(t, "usuarios.db")
+return 1
 end
