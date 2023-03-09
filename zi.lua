4ddabfbb7837f0bffd1e97600c75a2404e1d6040
kikomayorga <kikomayorga@gmail.com>
2023-03-09 02:26:09 -0500

    still fixing

--- zi.lua
+++ zi.lua
@@ -1,5 +1,7 @@
 os.execute("echo 1 > /tmp/zi/busyflag")
-os.execute("mpg123 /etc/zi/sounds/keypress.mp3")
+path = "/etc/"
+-- path = "~/Documents/" -- at my ubuntu
+os.execute("mpg123 "..path.."zi/sounds/keypress.mp3")
 
 require "tablesaveload" -- for persistance
 require "zi_functions"  -- custom functions by zi
@@ -11,11 +13,10 @@ users_db = "users_table.db"
 devices_db = "devices_table.db"
 states_db = "states_table.db"
 
--- on ubuntu
--- path = "~/Documents/" -- (DEV
--- on openwrt
-path = "/etc/"
 
+
+
+--[[
 if (arg[1] == "states" and arg[2] == "reset") then states_reset(states_db) end
 
 if arg[1] == "key"
@@ -102,50 +103,4 @@ then
 
 end
 
-
-
-
-
-
---[[
-if arg[1] == "hostapd"
-then
-end
-]]
-
---[[
-if arg[1] = nil
-then
-  print("zi.lua was called with no arguments.")
-end
-]]
-
--- print(string.format('Received argument "%s"',arg[1]))
--- print(string.format(arg[1]))
-
--- users_db_reset(users_db)
--- devices_db_reset(devices_db)
-
-
--- template: function rename_user(db_file, userID, new_name)
--- chosen_animal  = 3
--- rename_user(users_db, 1, animals_table[chosen_animal])
-
--- function change_password(db_file, userID, new_password)
--- new_password = "5634"
--- change_password(users_db, 1, new_password)
-
--- function assign_device_permanently(db_file, deviceNR, userID)
--- userID = 1
--- deviceNR = 10
--- assign_device_permanently(users_db, deviceNR, userID)
-
--- userID = 1
--- deviceNR = 5
--- disassign_device_permanently(users_db, deviceNR, userID)
-
---[[
-os.execute("pico2wave -w /tmp/pause1.wav -l es-ES \"Usuario 1. Pausa de internet. Aun tienes $saldo1 minutos.\"")
-os.execute("aplay -q -f S16_LE -D plughw:0,0 /tmp/pause1.wav")
-os.execute("mpg123 ~/Documents/zi/sounds/aplausos.mp3")  
 ]]
