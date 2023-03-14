5ebfae0cb6c9b56f62f296ed6f969a4b623cd8de
kikomayorga <kikomayorga@gmail.com>
2023-03-13 21:59:32 -0500

    maybe faster when  aplay is finished with &

--- onkeys/on_one.sh
+++ onkeys/on_one.sh
@@ -30,8 +30,8 @@ if [ "$busyflag" = "1" ] && [ "$skippableflag" = "1" ]
 then 
 	mpg123 /etc/zi/sounds/click.mp3
 	#kill current audio and process
-	killall -q aplay 
-	killall -q lua 
+	killall -q aplay &
+	killall -q lua
 	echo 0000 > /tmp/zi/last4keys
 	echo 0 > /tmp/zi/busyflag
 	cd /etc/zi && lua zi.lua key 1 0000
