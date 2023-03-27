16ee08dfa2b82745cee6de2f85d9437678f4bed1
kikomayorga <kikomayorga@gmail.com>
2023-03-27 18:40:42 -0500

    starting to fix loggd user menu

--- zi.lua
+++ zi.lua
@@ -111,18 +111,18 @@ if (get_state(states_db) == "iddle" and arg[1] == "key") then
     running = get_running_status(users_db, logged_user)
     os.execute("echo 0000 > /tmp/zi/last4keys")
     if running == 0 then
-      os.execute("aplay /tmp/zi/u_1.wav"..continue..
-        "pico2wave -w /tmp/zi/wav.wav -l es-ES \"<volume level='50'><pitch level=\'130\'>"..
+      os.execute('aplay /tmp/zi/u_1.wav'..
+        '&& pico2wave -w /tmp/zi/wav.wav -l es-ES " '..vol_pitch..' '..
         get_time_left_today(users_db, logged_user)..
-        "&& aplay -q -f U8 -r8000 -D plughw:0,0 /tmp/zi/wav.wav"..
-        "&& aplay /tmp/zi/u_2.wav")
+        '&& aplay -q -f U8 -r8000 -D plughw:0,0 /tmp/zi/wav.wav'..
+        '&& aplay /tmp/zi/u_2.wav')
     end
     if running == 1 then
-      os.execute("aplay /tmp/zi/u_3.wav"..
-        "&& pico2wave -w /tmp/zi/wav.wav -l es-ES \"<volume level='50'><pitch level=\'130\'>"..
+      os.execute('aplay /tmp/zi/u_1.wav'..
+        '&& pico2wave -w /tmp/zi/wav.wav -l es-ES " '..vol_pitch..' '..
         get_time_left_today(users_db, logged_user)..
-        "&& aplay -q -f U8 -r8000 -D plughw:0,0 /tmp/zi/wav.wav"..
-        "&& aplay /tmp/zi/u_3.wav")
+        '&& aplay -q -f U8 -r8000 -D plughw:0,0 /tmp/zi/wav.wav'..
+        '&& aplay /tmp/zi/u_3.wav')
     end  
     set_state(states_db, "user_menu")  -- sets statesmachine:
     set_logged_user(states_db, logged_user)        -- TODO:  is this needed?
