57491021d777ce40cd49433d9ea78235adaa61cb
kikomayorga <kikomayorga@gmail.com>
2023-04-08 01:30:16 -0500

    hostapd detection

--- disconnects/onhostapdchange.sh
+++ disconnects/onhostapdchange.sh
@@ -3,14 +3,16 @@
 if [[ $2 == "AP-STA-CONNECTED" ]]
 then
   echo "someone has connected with mac id $3 on $1"
+  cd /etc/zi/ && lua zi.lua hostapd $1 $2 $3
 fi
 
 if [[ $2 == "AP-STA-DISCONNECTED" ]]
 then 
   echo "someone has disconnected with mac id $3 on $1"
+  cd /etc/zi/ && lua zi.lua hostapd $1 $2 $3
 fi
 
-cd /etc/zi/ && lua zi.lua hostapd $1 $2 $3
+
 
 
 # how it went before:
